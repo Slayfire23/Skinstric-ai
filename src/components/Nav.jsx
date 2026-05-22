62487233653cc1ca4acd4afc4a2aff6243fb30ac
Slayfire23 <slayfire11@ymail.com>
2026-05-22 15:04:42 -0500

    Landing Page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <nav>
-      <h1>Skinstric</h1>
+    <nav className='fixed left-0 top-0 z-50 flex w-full items-center justify-between px-8 py-5'>
+
+      <div className='flex items-center gap-4'>
+        <h1 className='text-[14px] font-semibold text-black tracking-wider font-14px'>
+          SKINSTRIC
+        </h1>
+
+          <span className='text-[10px] font-semibold uppercase tracking-wide text-[#1a1b1c]'>
+            [ INTRO ]
+          </span>
+
+      </div>
+
+      <button className='bg-black px-4 py-2 text-[10px] font-semibold tracking-wide text-white '>
+        Enter Code
+      </button>
     </nav>
   )
 }
---
+++ src/components/ProceedButton.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import proceedbtn from "../assets/proceedbtn.svg";
+
+function ProceedButton({ onClick, onMouseEnter, onMouseLeave, isHidden }) {
+  return (
+    <div 
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+    className={`group fixed right-8 top-1/2 z-10 -translate-y-1/2 transition-opacity duration-300 ${
+        isHidden ? "pointer-events-none opacity-0" : "opacity-100"
+      }`}>
+      <div className="pointer-events-none absolute right-[-190px] top-1/2 h-[320px] w-[320px] -translate-y-1/2 rotate-45 border border-dotted border-gray-500"></div>
+      <div className="pointer-events-none absolute right-[-235px] top-1/2 h-[410px] w-[410px] -translate-y-1/2 rotate-45 border border-dotted border-gray-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100"></div>
+
+      <div className="pointer-events-none absolute right-[-280px] top-1/2 h-[500px] w-[500px] -translate-y-1/2 rotate-45 border border-dotted border-gray-300 opacity-0 transition-opacity duration-300 group-hover:opacity-100"></div>
+
+      <button
+        onClick={onClick}
+        className="relative z-10 flex items-center gap-4 bg-transparent p-0 text-[12px] font-semibold uppercase tracking-wide text-black"
+      >
+        <span>Take Test</span>
+
+        <span className="relative flex h-12 w-12 items-center justify-center">
+          <span className="absolute h-9 w-9 rotate-45 border border-black"></span>
+          <span className="absolute h-9 w-9 rotate-45 border border-dotted border-gray-400 opacity-0 transition-all duration-300 group-hover:h-7 group-hover:w-7 group-hover:opacity-100"></span>
+          <img
+            src={proceedbtn}
+            alt=""
+            className="relative z-10 h-3 w-3 translate-x-[1px] transition-transform duration-300 group-hover:scale-75"
+          />
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default ProceedButton;
